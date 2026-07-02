6d618c2b227577eab75440a4bdc7f9011207f9f2
Marcos Mourão <dev@mourao.info>
2026-07-02 18:26:10 -0300

    feat(player): Keep the player inbounds

--- Assets/_Project/_Scripts/PlayerController.cs
+++ Assets/_Project/_Scripts/PlayerController.cs
@@ -17,6 +17,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.x < -10.0f)
+        {
+            transform.position = new Vector3(-10.0f, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x > 10.0f)
+        {
+            transform.position = new Vector3(10.0f, transform.position.y, transform.position.z);
+        }
+
         moveInput = moveAction.ReadValue<Vector2>();
 
         transform.Translate(Vector3.right * moveInput.x * speed * Time.deltaTime);
