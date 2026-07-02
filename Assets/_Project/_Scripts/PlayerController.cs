0abca8d288f6a3bddcbbb929faa8d714e1783909
Marcos Mourão <dev@mourao.info>
2026-07-02 17:42:21 -0300

    feat(player):  Get Vector2 values for Input

--- Assets/_Project/_Scripts/PlayerController.cs
+++ Assets/_Project/_Scripts/PlayerController.cs
@@ -6,15 +6,16 @@ public class PlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     public InputAction moveAction;
+    public Vector2 moveInput;
     
     void Start()
     {
-        
+        moveAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        moveInput = moveAction.ReadValue<Vector2>();
     }
 }
