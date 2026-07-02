d88ec8d193f1ec2e8baf0f6258723f9d3b2ee9cc
Marcos Mourão <dev@mourao.info>
2026-07-02 17:24:47 -0300

    feat(player):  Configure a new moveAction variable
    Aadded a Move Action using Unity’s newer Input System

---
+++ Assets/_Project/_Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public InputAction moveAction;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
